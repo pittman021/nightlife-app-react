0282f42f30f0b0cc8095ab6ac7c31104de8d2f18
Tim Pittman <tim.pittman021@gmail.com>
2017-09-10 19:04:42 -0500

    fixing prod mongo keys

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@
 module.exports = {
   GOOGLE_CLIENT_ID:
     '936081905425-kah8hm08e1ug3m9evvlbtktnqbg56mgv.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: 'AGAy-2undjEMu8XOg2MGUIMr'
+  GOOGLE_CLIENT_SECRET: 'AGAy-2undjEMu8XOg2MGUIMr',
+  MONGO_URI: 'mongodb://tim:pass@ds123534.mlab.com:23534/nightlife-app-dev'
 };

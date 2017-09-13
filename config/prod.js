7837708bfe7b58c134645082385c41ad442f3d64
Tim Pittman <tim.pittman021@gmail.com>
2017-09-13 01:18:58 -0500

    fixing prod env vars

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+// export prod keys here //
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  yelpConsumerKey: process.env.YELP_CONSUMER_KEY,
+  yelpConsumerSecret: process.env.YELP_CONSUMER_SECRET,
+  yelpToken: process.env.YELP_TOKEN,
+  yelpTokenSecret: process.env.YELP_TOKEN_SECRET,
+  cookieKey: process.env.COOKIE_KEY
+};

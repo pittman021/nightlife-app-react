562098e0a58020312f572eada6f1b926b9ea3239
Tim Pittman <tim.pittman021@gmail.com>
2017-09-13 22:47:56 -0500

    struggles

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it doesn't recognize route?
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

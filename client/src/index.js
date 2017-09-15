019a2d0c3cbd48e68771ad03962841a64bc6fc63
Tim Pittman <tim.pittman021@gmail.com>
2017-09-15 00:09:20 -0500

    shit shit

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+
 import 'materialize-css/dist/css/materialize.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
